15fcbc6451e42a30c6dcbb59a4c82066f0b9ed49
Stanislav Bzhezinskyi <83300533+stanislav-bzhezinskyi@users.noreply.github.com>
2022-07-25 19:07:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
 'use script'
 
-const SignInLink = document.querySelector('.sign-in-link');
-const SignUpLink = document.querySelector('.sign-up-link');
-
-SignInLink.setAttribute('style', 'border-bottom: 2px solid yellow');
-SignUpLink.setAttribute('style', 'color: #9895ab');
-
-SignInLink.addEventListener('click', (event) => {
-    event.preventDefault();
-    SignInLink.setAttribute('style', 'border-bottom: 2px solid yellow; color: white');
-    SignUpLink.setAttribute('style', 'color: #9895ab');
-})
-
-SignUpLink.addEventListener('click', (event) => {
-    event.preventDefault();
-    SignUpLink.setAttribute('style', 'border-bottom: 2px solid yellow; color: white');
-    SignInLink.setAttribute('style', 'color: #9895ab');
-})
-
-const checkboxForm = document.querySelector('.checkbox-form');
-const checkboxCover = document.querySelector('.checkbox-cover');
-const checkbox = document.getElementById('checkbox');
-const checkboxLabel = document.querySelector('.checkbox-label');
-
-checkboxForm.addEventListener('click', () => {
-    if (!checkboxCover.hidden) {
-        return (
-            checkboxCover.setAttribute('hidden', true),
-            checkbox.checked = true
-        )
-    }
-
-    if (checkboxCover.hidden) {
-        return (
-            checkboxCover.removeAttribute('hidden'),
-            checkbox.checked = false
-        )
-    }
-})
-
+const signInLink = document.getElementById('sign-in-link');
+const signUpLink = document.getElementById('sign-up-link');
 const form = document.querySelector('.form');
 
+signUpLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    signInLink.classList.value = 'form__link';
+    signUpLink.classList.value = 'form__link active-link';
+})
+
+signInLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    signUpLink.classList.value = 'form__link';
+    signInLink.classList.value = 'form__link active-link';
+})
+
 form.addEventListener('submit', (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const data = new FormData(form);
-
     console.log(
         Object.fromEntries(data.entries())
     );
